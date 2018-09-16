d136074db5e3501416456d36cd05e66cb7d26d8f
Ashari Juang <asharijuang@me.com>
2018-09-16 15:14:02 +0700

    initiate project

---
+++ CodeChallenges/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CodeChallenges
+//
+//  Created by Qiscus on 16/09/18.
+//  Copyright © 2018 Qiscus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
